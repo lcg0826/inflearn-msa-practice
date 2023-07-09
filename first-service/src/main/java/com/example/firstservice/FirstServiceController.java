9355358f7b0d74f90292bafe13693e7543b1cbda
이찬규 <lcg082695@gmail.com>
2023-07-10 01:39:51 +0900

    라이브 코딩

--- first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,6 +1,9 @@
 package com.example.firstservice;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class FirstServiceController {
 
+    Environment env;
+
+    @Autowired
+    public FirstServiceController(Environment env) {
+        this.env = env;
+    }
+
     @GetMapping("/welcome")
     public String welcome() {
+
         return "welcome first service";
     }
 
@@ -23,7 +34,9 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return " hi, there. this is a message from first service";
+    public String check(HttpServletRequest request) {
+
+        log.info("Server port={}", request.getServerPort());
+        return String.format("hi, there. this is a message from first service on PORT %S", env.getProperty("local.server.port"));
     }
 }
