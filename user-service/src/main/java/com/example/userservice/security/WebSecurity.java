8abbfb31902daf46d1bc785bb615a6a91e4c84f4
changyu0826 <lcg082695@gmail.com>
2023-08-24 01:48:04 +0900

    web security 3.x.x 버전에 맞게 수정

--- user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -1,10 +1,9 @@
 package com.example.userservice.security;
-
 import com.example.userservice.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.ObjectPostProcessor;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -12,10 +11,50 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.IpAddressMatcher;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+public class WebSecurity {
+
+    private final UserService userService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final ObjectPostProcessor<Object> objectPostProcessor;
+    @Bean
+    protected SecurityFilterChain config(HttpSecurity http) throws Exception {
+        http.csrf((csrf) -> csrf.disable());
+        // AuthenticationFileter 추가 후
+//        http.authorizeHttpRequests(authorize -> authorize
+//                        .requestMatchers("/users/**", "/**")
+//                )
+//                .authorizeRequests().requestMatchers("/**")
+//                .access("hasIpAddress('"+"192.168.0.5"+"')")
+//                .and()
+        http.authorizeRequests(authorize -> authorize
+                .requestMatchers("/users/**").permitAll() // "/users/**" 패턴에 대한 권한 설정
+                .anyRequest().access("hasIpAddress('" + "192.168.0.5" + "')")) // 나머지 URL 패턴에 대한 IP 주소 기반 권한 설정
+                .addFilter(getAuthenticationFilter())
+        ;
+        return http.build();
+    }
+
+    public AuthenticationManager authenticationManager(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
+        return auth.build();
+    }
+
+    private AuthenticationFilter getAuthenticationFilter() throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        AuthenticationManagerBuilder builder = new AuthenticationManagerBuilder(objectPostProcessor);
+        authenticationFilter.setAuthenticationManager(authenticationManager(builder));
+        return authenticationFilter;
+    }
+
+}
+
+// 변경 전 코드 -> 추후에 "SecurityFilterChain" 에 대해서 알아볼 것
+/* 
 public class WebSecurity{
 
     private final UserService userService;
@@ -77,3 +116,6 @@ public class WebSecurity{
     }
 
 }
+
+
+ */
