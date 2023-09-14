3dd0d6b250bd56669f0924f8bcb71152a9bc28c6
changyu0826 <lcg082695@gmail.com>
2023-09-15 01:24:03 +0900

    no message

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -36,7 +36,13 @@ public class UserController {
 
     @GetMapping("/health_check")
     public String statue() {
-        return String.format("service on port %s", env.getProperty("local.server.port"));
+        return String.format("user service info"
+                + ", local.server.port = " + env.getProperty("local.server.port")
+                + ", server.port = " + env.getProperty("server.port")
+                + ", token secret = " + env.getProperty("token.secret")
+                + ", token.expiration_time = " + env.getProperty("token.expiration_time")
+
+        );
     }
 
     @GetMapping("/welcome")
