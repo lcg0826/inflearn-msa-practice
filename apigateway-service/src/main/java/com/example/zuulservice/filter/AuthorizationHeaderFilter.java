fb3f39945418539f9bb370666ee6ce5acb144b42
changyu0826 <lcg082695@gmail.com>
2023-09-20 02:04:36 +0900

    no message

--- apigateway-service/src/main/java/com/example/zuulservice/filter/AuthorizationHeaderFilter.java
+++ apigateway-service/src/main/java/com/example/zuulservice/filter/AuthorizationHeaderFilter.java
@@ -1,2 +1,32 @@
-package com.example.zuulservice.filter;public class AuthorizationHeaderFilter {
+package com.example.zuulservice.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.env.Environment;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@Slf4j
+public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
+    Environment env;
+
+    public AuthorizationHeaderFilter(Environment env) {
+        super(Config.class);
+        this.env = env;
+    }
+
+    public static class Config {
+        // put configuration properties here
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return ((exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+        });
+    }
 }
+
