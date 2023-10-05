4e110f8c00082a7865818eb767c26310c9750f3f
changyu0826 <lcg082695@gmail.com>
2023-10-06 01:00:23 +0900

    user import 잘못한거 다시 가져옴

--- user-service/src/main/java/com/example/userservice/security/AuthenticationFilter.java
+++ user-service/src/main/java/com/example/userservice/security/AuthenticationFilter.java
@@ -12,12 +12,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import io.jsonwebtoken.Jwts;
-import org.apache.catalina.User;
 import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.io.IOException;
