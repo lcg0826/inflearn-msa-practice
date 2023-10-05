d26466228166aec68b4d75a666772846dc770763
changyu0826 <lcg082695@gmail.com>
2023-10-06 01:46:29 +0900

    no message

--- user-service/src/main/java/com/example/userservice/jpa/UserRepository.java
+++ user-service/src/main/java/com/example/userservice/jpa/UserRepository.java
@@ -1,7 +1,9 @@
 package com.example.userservice.jpa;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
 
     UserEntity findByUserId(String userId);
