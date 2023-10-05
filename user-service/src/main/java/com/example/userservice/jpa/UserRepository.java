b360018028e874e9a0201a439c9b06848a2be3d1
changyu0826 <lcg082695@gmail.com>
2023-10-06 01:24:02 +0900

    no message

--- user-service/src/main/java/com/example/userservice/jpa/UserRepository.java
+++ user-service/src/main/java/com/example/userservice/jpa/UserRepository.java
@@ -6,5 +6,5 @@ public interface UserRepository extends CrudRepository<UserEntity, Long> {
 
     UserEntity findByUserId(String userId);
 
-    UserEntity findByEmail(String username);
+    UserEntity findByEmail(String email);
 }
